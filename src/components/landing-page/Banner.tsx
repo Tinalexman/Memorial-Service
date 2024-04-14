2dbbf4d226fb606ff915293f6498ce22b6e268cc
= <jadekitan23@gmail.com>
2024-04-14 19:37:34 +0100

    Adjusted the banner

--- src/components/landing-page/Banner.tsx
+++ src/components/landing-page/Banner.tsx
@@ -65,32 +65,32 @@ const Banner = () => {
       />
       <div className="w-full h-full px-[100px] md:px-[5%] py-[32px] md:py-[20px] absolute z-10 top-0 left-0 flex flex-col items-center">
         <Navbar />
-        <div className="w-full h-full flex flex-col justify-center items-center mt-24 md:mt-16  ">
+        <div className="w-full h-full flex flex-col justify-center items-center mt-[10vh] md:mt-16  ">
           <Image
             src={Picture}
             alt="picture frame"
-            className="w-[170px] md:w-[116px] h-auto"
+            className="w-[calc(18vh)] md:w-[116px] h-auto"
           />
-          <div className="w-full flex flex-col items-center gap-2 mt-[52px] md:mt-8">
-            <h3 className="font-light text-primary-10 text-[28px] md:text-[14px] leading-[42px] md:leading-[21px]">
+          <div className="w-full flex flex-col items-center mt-[5vh] md:mt-8">
+            <h3 className="font-light text-primary-10 text-[calc(3.5vh)] md:text-[14px] leading-[calc(4vh)] md:leading-[21px]">
               THE LATE
             </h3>
-            <h2 className="font-normal text-primary-10 text-[40px] md:text-[17px] leading-[60px] md:leading-[25.5px]">
+            <h2 className="font-normal text-primary-10 text-[calc(5vh)] md:text-[17px] leading-[calc(6vh)] md:leading-[25.5px]">
               VENERABLE PROFESSOR
             </h2>
-            <h1 className="text-primary-base font-extrabold text-[100px] md:text-[37px] leading-[100px] md:leading-[37px]">
+            <h1 className="text-primary-base font-extrabold text-[calc(10vh)] md:text-[37px] leading-[calc(10vh)] md:leading-[37px]">
               JACOB ADELEKE
             </h1>
-            <h1 className="text-primary-10 font-extrabold text-[216px] md:text-[87px] leading-[216px] md:leading-[87px]">
+            <h1 className="text-primary-10 font-extrabold text-[calc(23vh)] md:text-[87px] leading-[calc(23vh)] md:leading-[87px]">
               FAYOMI
             </h1>
           </div>
         </div>
-        <div className="mt-[52px] flex flex-col items-center gap-2 w-fit">
-          <h3 className="font-light text-primary-10 text-[20px] md:text-[16px] leading-[30px]">
+        <div className="mt-[2vh] flex flex-col items-center gap-1 w-fit">
+          <h3 className="font-light text-primary-10 text-[18px] md:text-[16px] leading-[26px]">
             Scroll to Navigate
           </h3>
-          <div className="w-[35px] h-[35px] bg-primary-border-40 rounded-full flex justify-center items-center">
+          <div className="w-[30px] h-[30px] bg-primary-border-40 rounded-full flex justify-center items-center">
             <motion.div
               animate={{
                 y: ["0%", "30%", "0%"],
